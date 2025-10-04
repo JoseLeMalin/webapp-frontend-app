bee4cbd5eb1c1742b87fb551a0720df652f38a1c
JoseLeMalin <jlm_488@tutamail.com>
2025-10-04 09:54:56 +0200

    save before pull

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,6 @@ export const Route = createFileRoute("/")({
   component: Home,
 });
 
-
 function Home() {
   /* const getTodos = async () => {
     const response = await fetch("http://localhost:5195/todoitems", {
@@ -13,8 +12,26 @@ function Home() {
     console.log(data);
   }; */
   const getTodos = async () => {
-    const response = await fetch("http://localhost:5221/api/user", {
+    const response = await fetch("http://localhost:5221/user", {
       method: "GET",
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+  const postUser = async () => {
+    const response = await fetch("http://localhost:5195/todoitems", {
+      method: "POST",
+      body: JSON.stringify({
+        Username: "usernameTest",
+        Email: "email@email.com",
+        FirstName: "José",
+        LastName: "LeMalin",
+        DateOfBirth: "01/01/1990",
+        PictureUrl: "url.com",
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
     const data = await response.json();
     console.log(data);
@@ -35,13 +52,19 @@ function Home() {
   };
 
   return (
-    <div className="p-2">
+    <div className="flex flex-row gap-4 p-2">
       <h3>Welcome Home!!!</h3>
       <button
         className="bg-blue-500 text-white px-4 py-2 rounded"
         onClick={getTodos}
       >
         Get Todos
+      </button>
+      <button
+        className="bg-blue-500 text-white px-4 py-2 rounded"
+        onClick={postUser}
+      >
+        Post User
       </button>
       <button
         className="bg-blue-500 text-white px-4 py-2 rounded"
